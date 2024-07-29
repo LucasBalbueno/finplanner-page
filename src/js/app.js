551829916c16fe1e39488c191e07c1a9bcc5b231
LucasBalbueno <balbuenolucas04@gmail.com>
2024-07-29 13:55:48 -0300

    Ajustando configurações dos forms

--- src/js/app.js
+++ src/js/app.js
@@ -2,25 +2,30 @@ const buttonOpen = document.getElementById('buttonAdd');
 const btnClose = document.getElementById('btnClose');
 const modalForms = document.getElementById('formsApp');
 
+// const buttonSubmit = document.getElementById('buttonSubmit');
 const formGanhos = document.getElementById('formAppGanhos');
 const formGastos = document.getElementById('formAppGastos');
 
-// Abre o forms
-buttonOpen.addEventListener('click', (event) => {
-    event.preventDefault();
+// function Abre o forms
+function openModal (event) {
+    // event.preventDefault();
     modalForms.classList.add('formActivated');
-});
+}
 
-// Fecha o forms
-btnClose.addEventListener('click', (event) => {
-    event.preventDefault();
+// function fecha o forms
+
+function closeModal (event) {
+    // event.preventDefault();
     modalForms.classList.remove('formActivated');
 
     clearInputs()
-});
+}
+
+buttonOpen.addEventListener('click', openModal)
+btnClose.addEventListener('click', closeModal)
 
 // Funcação para pegar os inputs na DOM baseado no name
-function getInputs () {
+function getInputs() {
     return {
         title: document.querySelector('[name="title"]'),
         infos: document.querySelector('[name="infos"]'),
@@ -30,7 +35,7 @@ function getInputs () {
 }
 
 // função para limpar os inputs
-function clearInputs () {
+function clearInputs() {
     const inputs = getInputs();
 
     inputs.title.value = '';
@@ -38,3 +43,53 @@ function clearInputs () {
     inputs.date.value = '';
     inputs.valor.value = '';
 }
+
+// Função para pegar os valores do inputs
+function getValueForm(idForm) {
+    const inputs = getInputs();
+
+    let type;
+
+    if (idForm === "formAppGanhos") {
+        type = 'ganho'
+    } else {
+        type = 'gasto'
+    }
+
+    const title = inputs.title.value;
+    const infos = inputs.infos.value;
+    const date = inputs.date.value;
+    const valor = inputs.valor.value;
+
+    if (!title || !infos || !date || !valor) {
+        alert('Por favor, preencha todos os campos para concluir a ação.');
+    } else {
+        return {type, title, infos, date, valor }
+    }
+}
+
+if (formGanhos) {
+    formGanhos.addEventListener('submit', (event) => {
+        event.preventDefault()
+
+        if (event.submitter.name === 'btnSubmitGanhos') {
+            const teste = getValueForm(event.target.id)
+            console.log(teste)
+        }
+
+        closeModal()
+    }
+)}
+
+if (formGastos) {
+    formGastos.addEventListener('submit', (event) => {
+        event.preventDefault()
+
+        if (event.submitter.name === 'btnSubmitGastos') {
+            const teste = getValueForm(event.target.id)
+            console.log(teste)
+        }
+
+        closeModal()
+    }
+)}
--- src/js/database.js
+++ src/js/database.js
@@ -1,22 +1,47 @@
 class FinanceNotes {
-    constructor (title, infos, date, valor) {
+    constructor (id, title, infos, date, valor) {
+        this.id = id;
         this.title = title;
         this.infos = infos;
         this.date = date;
         this.valor = valor;
     }
+
+    // função para validar dados
 }
 
 class DataBase {
+    constructor(){
+        const id = localStorage.getItem('id')
+
+        if(id === null){
+            localStorage.setItem('id', 0)
+        }
+    }
+
     createFinanceNote(financeNote) {
-        localStorage.setItem('ganhos', financeNote)
+        localStorage.setItem(financeNote.id, JSON.stringify(financeNote))
+    }
+
+    removeTask(id) {
+        localStorage.removeItem(id)
+    }
+
+    resetAll() {
+        localStorage.clear()
     }
 }
 
-const newNote = new FinanceNotes(('teste', 'infosteste', '14-07-05', 1500));
+const newNote = new FinanceNotes(1, 'teste', 'infosteste', '14-07-05', 1500);
+const newNote2 = new FinanceNotes(2, 'teste', 'infosteste', '14-07-05', 1500);
 
 const newData = new DataBase()
 
+// newData.resetAll()
+
 newData.createFinanceNote(newNote)
+newData.createFinanceNote(newNote2)
+
+// newData.removeTask(1)
 
 // ATÉ AQUI TUDO CERTO, LOCALSTORAGE MANTEM EM TODAS AS PÁGINAS
